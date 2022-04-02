e31fbfe51c7d72b1b0af9a95c9a0d5dac50786f7
Yuheng <miao.yuhe@northeastern.edu>
2022-04-01 20:56:30 -0700

    populate home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,20 +1,60 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import {Button, Carousel, Figure, Form, FormControl, Placeholder} from "react-bootstrap";
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import "../styles/Home.css";
 
-function Home(props) {
+function Home() {
     return (
         <div>
             <Navigation/>
-            <section id="search">
-                Search
-            </section>
-            <section id="headline">
-                Headline
-            </section>
-            <section id="about">
-                About
-            </section>
+
+            <main>
+                <section id="search">
+                    <Form className="search-area">
+                        <FormControl className="search-box" aria-label="Type the name of your professor"/>
+                        <Button variant="outline-dark" type="submit">
+                            <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass}/>
+                        </Button>
+                    </Form>
+                </section>
+
+                <section id="headline">
+                    {/*TODO: Map top five professors*/}
+                    <Carousel className="headline-img">
+                        <Carousel.Item>
+                            <img
+                                className="d-block w-100"
+                                src="http://hwr.org.uk/wp-content/uploads/2016/11/placeholder-dark-600-400-729dad18518ecd2cd47afb63f9e6eb09.jpg"
+                                alt="First slide"
+                            />
+                            <Carousel.Caption>
+                                <h3>First slide label</h3>
+                                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                            </Carousel.Caption>
+                        </Carousel.Item>
+                    </Carousel>
+                </section>
+
+                <section id="about">
+                    <Figure className="neu-video">
+                        <Figure.Image
+                            alt="neu video"
+                            src="https://www.addictivetips.com/app/uploads/2018/12/YouTube-Screenshot-Button.jpg"
+                        />
+                        <Figure.Caption>
+                            Nulla vitae elit libero, a pharetra augue mollis interdum.
+                        </Figure.Caption>
+                    </Figure>
+                    <div className="neu-description">
+                        <Placeholder xs={6}/>
+                        <Placeholder className="w-75"/> <Placeholder style={{width: '25%'}}/>
+                    </div>
+                </section>
+            </main>
+
             <Footer/>
         </div>
     );
