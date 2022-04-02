21de40ee2b6c995d4fa6684d8ad4c59f23d87aea
Yuheng <miao.yuhe@northeastern.edu>
2022-04-01 22:21:27 -0700

    draft search results with fake data

--- client/src/pages/SearchResults.js
+++ client/src/pages/SearchResults.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import Navigation from "../components/Navigation";
 import Footer from "../components/Footer";
+import {Card, Col, Row} from "react-bootstrap";
+import "../styles/SearchResults.css";
 
 function SearchResults(props) {
     return (
         <div>
-            <Navigation />
-            <Footer />
+            <Navigation/>
+
+            <main>
+                {/*TODO: map queried results*/}
+                {/*TODO: add pagination if have time*/}
+                <Row lg={1} className="g-4 search-results">
+
+                    <h1>Search Results ...</h1>
+
+                    {Array.from({length: 4}).map((_, idx) => (
+                        <Col key={idx}>
+                            <Card className="search-result">
+                                <Card.Body className="search-result-rating">
+                                    <Card.Text as="h2">{idx}</Card.Text>
+                                </Card.Body>
+                                <Card.Body className={"search-result-content"}>
+                                    <Card.Title>{`Neda ${idx}`}</Card.Title>
+                                    <Card.Text>
+                                        {"Descriptions: The household registration is also integral to the state’s control capacity. It splits the population into subcategories, divides the working population, and prevents both urban-rural and broad working-class solidarity."}
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                        </Col>
+                    ))}
+                </Row>
+            </main>
+
+            <Footer/>
         </div>
     );
 }
