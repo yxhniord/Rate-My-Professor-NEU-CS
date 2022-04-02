1518e30903afe51eb93e2cd8b7f300941bb56be3
Yuheng <miao.yuhe@northeastern.edu>
2022-04-01 19:01:23 -0700

    add paths

--- client/src/index.js
+++ client/src/index.js
@@ -1,11 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 import './index.css';
-import App from './App';
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Profile from "./pages/Profile";
+import SearchResults from "./pages/SearchResults";
+import RatingDetails from "./pages/RatingDetails";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <React.StrictMode>
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/profile" element={<Profile/>}/>
+                <Route path="/search/:query" element={<SearchResults/>}/>
+                <Route path="/details/:profId" element={<RatingDetails/>}/>
+            </Routes>
+        </BrowserRouter>
+    </React.StrictMode>,
+    document.getElementById('root')
 );
