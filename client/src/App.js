d3e97ade42fd21ca9540650883996f488ca14ab9
Zhiwei Bao <zhiv.bao@gmail.com>
2022-04-10 18:47:30 -0700

    Protect user profile with authentication required

--- client/src/App.js
+++ client/src/App.js
@@ -11,24 +11,25 @@ import Error from "./pages/Error";
 import React from "react";
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
+import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-  return (
-    <div className="App">
-        <Navigation/>
-        <Routes>
-            <Route path="/" element={<Home/>}/>
-            <Route path="/login" element={<Login/>}/>
-            <Route path="/signup" element={<Signup/>}/>
-            <Route path="/profile" element={<Profile/>}/>
-            <Route path="/search/:name" element={<SearchResults/>}/>
-            <Route path="/details/:profId" element={<RatingDetails/>}/>
-            <Route path="/newComment/:profId" element={<NewComment/>}/>
-            <Route path="/error" element={<Error/>}/>
-        </Routes>
-        <Footer/>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navigation/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/login" element={<Login/>}/>
+                <Route path="/signup" element={<Signup/>}/>
+                <Route path="/profile" element={<ProtectedRoute protectedComponent={Profile}/>}/>
+                <Route path="/search/:name" element={<SearchResults/>}/>
+                <Route path="/details/:profId" element={<RatingDetails/>}/>
+                <Route path="/newComment/:profId" element={<NewComment/>}/>
+                <Route path="/error" element={<Error/>}/>
+            </Routes>
+            <Footer/>
+        </div>
+    );
 }
 
 export default App;
