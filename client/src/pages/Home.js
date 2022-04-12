1fb10304ad039f2732c968ea00ec2b7ea50d70dd
Yuheng <miao.yuhe@northeastern.edu>
2022-04-11 17:43:44 -0700

    add mobile responsiveness and youtube api

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useState} from 'react';
 import {useNavigate} from "react-router-dom";
-import {Button, Carousel, Figure, Form, FormControl, Placeholder, Spinner} from "react-bootstrap";
+import {Button, Carousel, Col, Container, Form, FormControl, Row, Spinner} from "react-bootstrap";
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
 import "../styles/Home.css";
@@ -56,70 +56,77 @@ function Home() {
     };
 
     return (
-        <div>
 
-            <main>
-                <section id="search">
-                    <Form className="search-area" onSubmit={handleSubmit}>
-                        <FormControl className="search-box" aria-label="Type the name of your professor" value={name}
-                                     onChange={(e) => setName(e.target.value)}/>
-                        <Button variant="outline-dark" type="submit">
-                            <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass}/>
-                        </Button>
-                    </Form>
-                </section>
+        <main>
+            <section id="search">
+                <Form className="search-area" onSubmit={handleSubmit}>
+                    <FormControl className="search-box" aria-label="Type the name of your professor" value={name}
+                                 onChange={(e) => setName(e.target.value)}/>
+                    <Button variant="outline-dark" type="submit">
+                        <FontAwesomeIcon className="search-icon" icon={faMagnifyingGlass}/>
+                    </Button>
+                </Form>
+            </section>
 
-                {isLoading ?
-                    <Spinner animation="border" role="status">
-                        <span className="visually-hidden">Loading...</span>
-                    </Spinner> :
-                    <>
-                        {isAuthenticated && <section id="headline">
-                            {loading ?
-                                <Spinner animation="border" role="status">
-                                    <span className="visually-hidden">Loading...</span>
-                                </Spinner> :
-                                <>
-                                    {comments.length > 0 && <Carousel className="headline-img">
-                                        {comments.map((comment, index) => {
-                                            return (
-                                                <Carousel.Item key={comment._id} >
-                                                    <img
-                                                        className="d-block w-100"
-                                                        src="https://media.istockphoto.com/photos/old-school-chalkboard-picture-id547016978?b=1&k=20&m=547016978&s=170667a&w=0&h=CFpK3c30n2dD059xLC0PxngaX1wMn2Aa5erw9M0ub3s="
-                                                        alt="Carousel background image"
-                                                    />
-                                                    <Carousel.Caption className="headline-caption" >
-                                                        <h3>{professors[index]?.first_name} {professors[index]?.last_name}</h3>
-                                                        <br/>
-                                                        <p>{comment.course && `Course: ${comment.course}`}</p>
-                                                        <p>{comment.rate && `Rating: ${comment.rate}`}</p>
-                                                        <p>{comment.content && `Comment: ${comment.content}`}</p>
-                                                    </Carousel.Caption>
-                                                </Carousel.Item>)
-                                        })}
-                                    </Carousel>}
-                                </>
-                            }
-                        </section>}
-                    </>
-                }
+            {isLoading ?
+                <Spinner animation="border" role="status">
+                    <span className="visually-hidden">Loading...</span>
+                </Spinner> :
+                <>
+                    {isAuthenticated && <section id="headline">
+                        {loading ?
+                            <Spinner animation="border" role="status">
+                                <span className="visually-hidden">Loading...</span>
+                            </Spinner> :
+                            <>
+                                {comments.length > 0 && <Carousel className="headline-img">
+                                    {comments.map((comment, index) => {
+                                        return (
+                                            <Carousel.Item key={comment._id}>
+                                                <img
+                                                    className="d-block w-100"
+                                                    src="https://media.istockphoto.com/photos/old-school-chalkboard-picture-id547016978?b=1&k=20&m=547016978&s=170667a&w=0&h=CFpK3c30n2dD059xLC0PxngaX1wMn2Aa5erw9M0ub3s="
+                                                    alt="Carousel background image"
+                                                />
+                                                <Carousel.Caption className="headline-caption">
+                                                    <h3>{professors[index]?.first_name} {professors[index]?.last_name}</h3>
+                                                    <br/>
+                                                    <p>{comment.course && `Course: ${comment.course}`}</p>
+                                                    <p>{comment.rate && `Rating: ${comment.rate}`}</p>
+                                                    <p>{comment.content && `Comment: ${comment.content}`}</p>
+                                                </Carousel.Caption>
+                                            </Carousel.Item>)
+                                    })}
+                                </Carousel>}
+                            </>
+                        }
+                    </section>}
+                </>
+            }
 
-                <section id="about">
-                    <div className="neu-video">
-                        <iframe   src="https://www.youtube.com/embed/HwBcOli0YLM"
-                                 title="YouTube video player" frameBorder="0"
-                                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                                 allowFullScreen/>
-                    </div>
-                    <div className="neu-description">
-                        <Placeholder xs={6}/>
-                        <Placeholder className="w-75"/> <Placeholder style={{width: '25%'}}/>
-                    </div>
-                </section>
-            </main>
-
-        </div>
+            <section id="about">
+                <Container fluid className="about-container">
+                    <Row>
+                        <Col as="Container" xs={12} md={6} className="neu-video">
+                            <iframe src="https://www.youtube.com/embed/HwBcOli0YLM"
+                                    title="YouTube video player" frameBorder="0"
+                                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                                    allowFullScreen/>
+                        </Col>
+                        <Col as="Container" xs={12} md={6} className="neu-description">
+                            <a href="https://www.northeastern.edu/experience/">
+                                <h3>Northeastern University</h3>
+                            </a>
+                            <h6>Experience is at the core of all we do.</h6>
+                            <p>It’s what you gain when you
+                                make the world your classroom, your laboratory, and your platform to create change
+                                or grow your enterprise. To find ways of doing things differently, and better. And to
+                                seize opportunities as they unfold—anytime, anywhere.</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        </main>
     );
 }
 
