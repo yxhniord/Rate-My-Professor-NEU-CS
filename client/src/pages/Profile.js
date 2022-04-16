a6937dd0f28b22fb07a6cb5152819b1ae44cda54
Zhiwei Bao <zhiv.bao@gmail.com>
2022-04-16 13:51:30 -0700

    Add delete comment button

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -4,13 +4,14 @@ import CommentList from "../components/CommentList";
 import "../styles/Profile.css"
 import {useAuth0} from "@auth0/auth0-react";
 import {useNavigate} from "react-router-dom";
-import {fetchDbUser} from "../function/Api.js";
+import {fetchDbUser, deleteCommentByCommentId, fetchCommentsByUserId} from "../function/Api.js";
 
 function Profile() {
     const {isAuthenticated, isLoading, user, getAccessTokenSilently} = useAuth0();
     const [loading, setLoading] = useState(true);
     const baseURL = process.env.REACT_APP_BASE_URL;
     const [dbUser, setDbUser] = useState(null);
+    const [comments, setComments] = useState([]);
     const navigate = useNavigate();
 
 
@@ -23,6 +24,10 @@ function Profile() {
                         navigate("/userInfoForm");
                     } else {
                         setDbUser(dbUsers[0]);
+                        fetchCommentsByUserId(baseURL, dbUsers[0]._id, token)
+                            .then(data => {
+                                setComments(data);
+                            })
                         setLoading(false);
                     }
                 });
@@ -35,7 +40,31 @@ function Profile() {
                     navigate("/error");
                 });
         }
-    }, [isLoading, isAuthenticated]);
+    }, [isLoading, isAuthenticated, comments.length]);
+
+    const deleteComment = (commentId) => {
+        setLoading(true);
+        getAccessTokenSilently()
+            .then(token => {
+                deleteCommentByCommentId(baseURL, commentId, token)
+                    .then(res => {
+                        if (res) {
+                            setComments(comments.filter(comment => comment._id !== commentId));
+                            setLoading(false);
+                        } else {
+                            console.log("Error deleting comment");
+                        }
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        navigate("/error");
+                    });
+            })
+            .catch(err => {
+                console.log(err);
+                navigate("/error");
+            });
+    };
 
     return (
         <div>
@@ -62,7 +91,7 @@ function Profile() {
                             }
                         </Tab>
                         <Tab eventKey="user-comments" title="My Ratings">
-                            {dbUser != null && <CommentList userId={dbUser._id}/>}
+                            {comments.length !== 0 && <CommentList comments={comments} deleteComment={deleteComment}/>}
                         </Tab>
                     </Tabs>
                 </div>
