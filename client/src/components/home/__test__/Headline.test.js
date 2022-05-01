ef3837a80d1934f8bf8ceef80ffc7dd216102637
Yuheng <miao.yuhe@northeastern.edu>
2022-05-01 14:03:47 -0700

    add test to headline

--- client/src/components/home/__test__/Headline.test.js
+++ client/src/components/home/__test__/Headline.test.js
@@ -1,5 +1,27 @@
 import {render, screen} from '@testing-library/react';
+import Headline from "../Headline";
 
-test('',()=>{
+test('Test headline texts',()=>{
+    const professorTest = [{
+        _id: 1,
+        first_name: "Test First Name",
+        last_name: "Test Last Name",
+        rate: 0
+    }];
+
+    const commentTest = [{
+        _id: 1,
+        course: "Test Course",
+        rate: 0,
+        content: "Test Content"
+    }];
+
+    const dbUserTest = {
+        nickname: "User Test"
+    };
+
+    render(<Headline loading={false} dbUser={dbUserTest} professors={professorTest} comments={commentTest}/>)
+    expect(screen.getByText(`Course: Test Course`)).toBeInTheDocument();
+    expect(screen.getByText(`Comment: Test Content`)).toBeInTheDocument();
 
 });
--- client/src/components/home/__test__/Search.test.js
+++ client/src/components/home/__test__/Search.test.js
@@ -2,6 +2,7 @@ import {fireEvent, render, screen} from '@testing-library/react';
 import Search from "../Search";
 
 test('Search field display text with inputs', () => {
+
     const setNameTest = jest.fn((value) => {
     })
     const {queryByPlaceholderText} = render(<Search setName={setNameTest}/>)
