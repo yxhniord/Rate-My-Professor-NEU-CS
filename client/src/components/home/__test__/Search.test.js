bd834ae46e7e0e3c30f93fddf0b1a5367ccd2afb
Yuheng <miao.yuhe@northeastern.edu>
2022-05-01 12:56:04 -0700

    add test to search bar

--- client/src/components/home/__test__/Search.test.js
+++ client/src/components/home/__test__/Search.test.js
@@ -1,5 +1,14 @@
-import {render, screen} from '@testing-library/react';
+import {fireEvent, render, screen} from '@testing-library/react';
+import Search from "../Search";
 
-test('',()=>{
+test('Search field display text with inputs', () => {
+    const setNameTest = jest.fn((value) => {
+    })
+    const {queryByPlaceholderText} = render(<Search setName={setNameTest}/>)
 
+    const searchInput = queryByPlaceholderText('Type professor\'s name')
+
+    fireEvent.change(searchInput, {target: {value: 'test'}})
+
+    expect(searchInput.value).toBe('test');
 });
